eddf74ef337f3fe357e2659cc2250c996139f754
HectorUlisesStamatio <ulisesstamatio@gmail.com>
2022-09-07 14:03:48 -0500

    [UPDATE] GetAll & GetByID

--- books-api/index.js
+++ books-api/index.js
@@ -1,3 +1,4 @@
+const { application, response } = require('express');
 const express = require('express');
 
 const app = express();
@@ -12,6 +13,16 @@ const books=[
 
 app.get('/',(req,resp) =>{
     resp.send('Welcome to Study Automation to learn REST API with Node JS')
+})
+
+app.get('/api/books', (req,resp) => {
+    resp.send(books)
+})
+
+app.get('/api/books/:id', (req,resp) => {
+    const book = books.find(v => v.id === parseInt(req.params.id));
+    if(!book) resp.status(404).send('Book not found')
+    resp.send(book)
 })
 
 app.listen(8080)
